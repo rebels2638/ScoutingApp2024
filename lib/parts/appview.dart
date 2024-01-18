5eb8edadb7ae87ebebcdc4cd47fa89bf76d2f05d
exoad <jackmeng0814@gmail.com>
2024-01-18 16:29:08 -0500

    `add` first commit

---
+++ lib/parts/appview.dart
@@ -0,0 +1,17 @@
+import "package:flutter/material.dart";
+
+
+class AppView extends StatefulWidget {
+  const AppView({super.key});
+
+  @override
+  State<AppView> createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
