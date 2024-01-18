93cb3edd0daa22bf6f57f048de36e4a16b160fea
exoad <jackmeng0814@gmail.com>
2024-01-18 16:29:08 -0500

    `add` first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text("2638 Scouting")),
+        body: const Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
