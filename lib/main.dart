0a7c55e0dc14f9a44fb1098e8369fddd65f54d40
exoad <jackmeng0814@gmail.com>
2024-01-21 21:36:05 -0500

    `add` web build ci

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:scouting_app_2024/debug.dart';
 import 'package:scouting_app_2024/parts/appview.dart';
 import 'package:scouting_app_2024/user/user_telemetry.dart';
@@ -10,16 +11,19 @@ void main() {
   // nothing should go above this comment besides the DateTime check
   Get.put<UserTelemetry>(UserTelemetry());
   WidgetsFlutterBinding.ensureInitialized();
+  GetStorage.init("RebelRobotics2638UserPreferenceTelemetryUnit");
   FlutterError.onError = (FlutterErrorDetails details) {
     FlutterError.presentError.call(details);
     Debug().warn(details.summary);
   };
   Debug().init();
+  UserTelemetry().reset();
+  UserTelemetry().init();
   Bloc.observer = const DebugObserver();
   const ThemedAppBundle app = ThemedAppBundle();
   runApp(app);
   Debug().info(
       "Took ${DateTime.now().millisecondsSinceEpoch - now.millisecondsSinceEpoch} ms to launch the app...");
   Debug().info(
-      "UserTelemetry Receiver Type: ${UserTelemetry.device().getKeys().runtimeType} with keys.length=${UserTelemetry.device().getKeys().length} and values.length=${UserTelemetry.device().getValues().length}");
+      "UserTelemetry Receiver Type: ${UserTelemetry.device.getKeys().runtimeType} with keys.length=${UserTelemetry.device.getKeys().length} and values.length=${UserTelemetry.device.getValues().length}");
 }
--- lib/user/user_telemetry.dart
+++ lib/user/user_telemetry.dart
@@ -1,30 +1,49 @@
+import 'dart:async';
 import 'package:get_storage/get_storage.dart';
 import 'package:json_annotation/json_annotation.dart';
+import 'package:scouting_app_2024/debug.dart';
 import 'package:scouting_app_2024/parts/theme.dart';
 
 part "user_telemetry.g.dart";
 
 /// User Telemetry storage is based on MVC patterning
 class UserTelemetry {
-  static GetStorage device() =>
+  static GetStorage device =
       GetStorage("RebelRobotics2638UserPreferenceTelemetryUnit");
   static final UserTelemetry _singleton = UserTelemetry._();
   factory UserTelemetry() => _singleton;
   UserTelemetry._();
 
-  static late UserPrefModel currentModel;
+  static late UserPrefModel _currentModel;
 
   bool isEmpty() =>
-      device().getKeys().length == 0 ||
-      device().getValues().length ==
+      device.getKeys().length == 0 ||
+      device.getValues().length ==
           0; // i feel like this is really bad
 
-  void init() {}
+  void init() {
+    Timer.periodic(const Duration(seconds: 10), (Timer timer) async {
+      Debug().info(
+          "USER_TELEMETRY Saving UserTelemetry Model now. Values: ${_currentModel.toJson()}");
+      await save();
+      Debug().info(
+          "UserTelemetry Receiver Type: ${UserTelemetry.device.getKeys().runtimeType} with keys.length=${UserTelemetry.device.getKeys().length} and values.length=${UserTelemetry.device.getValues().length}");
+    });
+  }
 
   /// resets the model, but does not perform a save
-  void reset() => currentModel = UserPrefModel.defaultModel;
+  void reset() => _currentModel = UserPrefModel.defaultModel;
 
-  Future<void> save() async => await device().save();
+  Future<void> save() async {
+    for (MapEntry<String, dynamic> entry
+        in _currentModel.toJson().entries) {
+      Debug()
+          .info("USER_TELEMETRY Writing ${entry.key}=${entry.value}");
+      await device.write(entry.key, entry.value.toString());
+    }
+    // is this even necessary??? idek lets leave it here so we absolutely sure
+    await device.save();
+  }
 }
 
 // we dont really store this as json, i just want to have the easy to use mapping feature lmao
