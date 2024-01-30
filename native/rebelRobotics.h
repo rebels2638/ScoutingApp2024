05b685acd57fff18cf1a9c04f2c3c5d1cb653b19
exoad <jackmeng0814@gmail.com>
2024-01-30 18:47:59 -0500

    `rmf` no more nix support, no pt

--- native/rebelRobotics.h
+++ native/rebelRobotics.h
@@ -3,9 +3,6 @@
 
 namespace RebelRobotics
 {
-  const char* LINUX_WINDOW_TITLE = "2638 Scouting";
-  const char* LINUX_WINDOW_ICON512 = "./assets/appicon_header.png";
-
   const wchar_t* WIN32_WINDOW_TITLE = L"2638 Scouting";
   const int WINDOW_WIDTH = 1200;
   const int WINDOW_HEIGHT = 740;
