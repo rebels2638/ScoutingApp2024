70a77b5a24842be57d6fa1f9bf5177f4bbcf44ca
chiming wang <chimingwang0@gmail.com>
2024-02-15 15:41:30 -0500

    reformat

--- lib/parts/views/game_info.dart
+++ lib/parts/views/game_info.dart
@@ -74,12 +74,12 @@ class GameInfoView extends StatelessWidget implements AppPageViewExporter {
   void _showDetails(BuildContext context, String title) {
     final Map<String, String> details = <String, String>{
       'Starting Zone': 'Where robots start at the beginning of the match before the autonomous period.\n\nAutonomous:\n• 2pts: Robot leaves starting zone',
-      'Stage': '• During the match, robots can score additional points by parking in their Stage Zone, getting Onstage via a chain, or scoring a Note in a Trap. \n \nPoints vary for being Onstage with or without the Spotlight per robot (3 points without Spotlight, 4 points with Spotlight during Teleop phase), and a note scored in a trap on stage are 5 points. \n \nHarmony is achieved when more than one robot is Onstage via the same chain, giving 2 ranking points.',
+      'Stage': '• During the match, robots can score additional points by parking in their Stage Zone, getting Onstage via a chain, or scoring a Note in a Trap. \n \nPoints vary for being Onstage with or without the Spotlight per robot.\n• 3pts: Spotlight\n• 4pts: Spotlight during Teleop phase)\n• 5pts: Note scored in a trap on stage.\n\n• Harmony is achieved when more than one robot is Onstage via the same chain, giving 2 ranking points.',
       'Source Area': 'Where Notes are introduced into the field for robots to collect.',
       'Amp Zone': 'During Auto and Tele-op, robots score Notes in the Amp zone, which can give benefits.\n\n• For 2 notes scored in Amp: human players can activate a 10 sec points multiplier for Notes scored in their Speaker\n\nCoopertition:\n• Each alliance scores 1 in Amp and presses Coopertition button\n• 1 Coopertition point (Ranking Point)\n• Reduced point requirement for Melody/bonus point\n\nAuto:\n• 2pts: Note scored in Amp\n\nTele-op:\n• 1pts: Note scored in Amp\n• 5pts: Note scored in Speaker (after Amp activation)',
       'Speaker': 'Notes are scored in the Speaker with different points awarded based on whether they are Amplified or not.\n\nAutonomous:\n• 5pts: Normal Note\n\nTele-op:\n• 2pts: Normal Note\n• 5pts: Amped Note',
-      'High Notes': 'High note - special game piece\n\nWhen a High Note is scored on a Microphone when one robot or more is on stage, it Spotlights Robots Onstage, granting one additional point per high note scored (max of 3).',
-      'Notes': 'Note - game piece\n\nThe primary game piece scored in the Speaker and Amp for points.\n\nIf a Note is placed in a Trap, it gives a maximum of one per Trap, awarding 5 points during Teleop.',
+      'High Notes': '• High note - special game piece\n\n• Spotlighting - when High Note scored on Microphone when 1 or more robots are on stage.\n• During Spotlight, every note scored in the trap will be granted one additional point (max 3) .',
+      'Notes': '• Note - primary game piece\n• Scored in Speaker and Amp for points',
     };
 
     final Map<String, IconData> icons = <String, IconData>{
