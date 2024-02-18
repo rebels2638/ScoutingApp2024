6ba12aab8a8d5f4b6b052f928c3edefc4a7d19d9
exoad <jackmeng0814@gmail.com>
2024-02-17 20:35:35 -0500

    `fix` some minor edits in accordance to the latest history

--- native/rebelRobotics.h
+++ native/rebelRobotics.h
@@ -1,12 +1,9 @@
 #ifndef REBEL_ROBOTICS
 #define REBEL_ROBOTICS
-
 namespace RebelRobotics
 {
   const wchar_t* WIN32_WINDOW_TITLE = L"2638 Scouting";
   const int WINDOW_WIDTH = 1200;
   const int WINDOW_HEIGHT = 740;
 } // namespace RebelRobotics
-
-
 #endif
