318502d511f491dea4c7d8c2ccefb1f6ba39e47f
exoad <jackmeng0814@gmail.com>
2024-03-21 18:54:35 -0400

    `fix` refactor src for windows

--- windows/runner/resource.h
+++ windows/runner/resource.h
@@ -1,11 +1,4 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by Runner.rc
-//
 #define IDI_APP_ICON                    101
-
-// Next default values for new objects
-//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
