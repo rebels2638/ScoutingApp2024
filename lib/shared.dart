d19bafff6f7a00835356e25a7c940233fe3d171d
exoad <jackmeng0814@gmail.com>
2024-03-21 19:09:06 -0400

    `add` amogus

--- lib/shared.dart
+++ lib/shared.dart
@@ -29,8 +29,8 @@ const int REBEL_ROBOTICS_APP_VERSION =
 const String REBEL_ROBOTICS_APP_NAME =
     "2638 Scouting \"$APP_CANONICAL_NAME\"";
 const String REBEL_ROBOTICS_APP_LEGALESE =
-    "Copyright (c) 2024, Jack Meng (exoad) and Chiming Wang (2bf) All rights reserved.";
+    "Copyright (c) 2024, Jack Meng (exoad). All rights reserved.";
 const String REBEL_ROBOTICS_APP_GITHUB_REPO_URL =
     "https://github.com/rebels2638/ScoutingApp2024";
-const String REBEL_ROBOTICS_APP_TUTORIAL_VIDEO_URL = 
+const String REBEL_ROBOTICS_APP_TUTORIAL_VIDEO_URL =
     "https://www.youtube.com/watch?v=-5jyO0sAhnA";
--- native/rebelRobotics.h
+++ native/rebelRobotics.h
@@ -2,7 +2,7 @@
 #define REBEL_ROBOTICS
 namespace RebelRobotics
 {
-  const wchar_t* WIN32_WINDOW_TITLE = L"2638 Scouting";
+  const wchar_t* WIN32_WINDOW_TITLE = L"Argus ~ Rebel Robotics";
   const int WINDOW_WIDTH = 1200;
   const int WINDOW_HEIGHT = 740;
 } // namespace RebelRobotics
