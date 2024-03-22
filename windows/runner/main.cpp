6f4cc8b8b55076736df2f12fc7147ccb082ed077
exoad <jackmeng0814@gmail.com>
2024-03-21 20:05:17 -0400

    `fix` windows versioning and build src

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -1,15 +1,19 @@
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
 #include <windows.h>
+
 #include "../../native/rebelRobotics.h"
 #include "flutter_window.h"
 #include "utils.h"
+
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
     CreateAndAttachConsole();
   }
+
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+
   flutter::DartProject project(L"data");
   std::vector<std::string> command_line_arguments = GetCommandLineArguments();
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
--- windows/runner/utils.cpp
+++ windows/runner/utils.cpp
@@ -1,12 +1,15 @@
 #include "utils.h"
+
 #include <flutter_windows.h>
 #include <io.h>
 #include <stdio.h>
 #include <windows.h>
+
 #include <iostream>
+
 void CreateAndAttachConsole() {
   if (::AllocConsole()) {
-    FILE* unused;
+    FILE *unused;
     if (freopen_s(&unused, "CONOUT$", "w", stdout)) {
       _dup2(_fileno(stdout), 1);
     }
@@ -17,27 +20,33 @@ void CreateAndAttachConsole() {
     FlutterDesktopResyncOutputStreams();
   }
 }
+
 std::vector<std::string> GetCommandLineArguments() {
   int argc;
   wchar_t** argv = ::CommandLineToArgvW(::GetCommandLineW(), &argc);
   if (argv == nullptr) {
     return std::vector<std::string>();
   }
+
   std::vector<std::string> command_line_arguments;
+
   for (int i = 1; i < argc; i++) {
     command_line_arguments.push_back(Utf8FromUtf16(argv[i]));
   }
+
   ::LocalFree(argv);
+
   return command_line_arguments;
 }
+
 std::string Utf8FromUtf16(const wchar_t* utf16_string) {
   if (utf16_string == nullptr) {
     return std::string();
   }
   int target_length = ::WideCharToMultiByte(
-    CP_UTF8, WC_ERR_INVALID_CHARS, utf16_string,
-    -1, nullptr, 0, nullptr, nullptr)
-    - 1;
+      CP_UTF8, WC_ERR_INVALID_CHARS, utf16_string,
+      -1, nullptr, 0, nullptr, nullptr)
+    -1; // remove the trailing null character
   int input_length = (int)wcslen(utf16_string);
   std::string utf8_string;
   if (target_length <= 0 || target_length > utf8_string.max_size()) {
@@ -45,8 +54,8 @@ std::string Utf8FromUtf16(const wchar_t* utf16_string) {
   }
   utf8_string.resize(target_length);
   int converted_length = ::WideCharToMultiByte(
-    CP_UTF8, WC_ERR_INVALID_CHARS, utf16_string,
-    input_length, utf8_string.data(), target_length, nullptr, nullptr);
+      CP_UTF8, WC_ERR_INVALID_CHARS, utf16_string,
+      input_length, utf8_string.data(), target_length, nullptr, nullptr);
   if (converted_length == 0) {
     return std::string();
   }
