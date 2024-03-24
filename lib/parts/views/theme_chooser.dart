c2b2204f4326e33d9ad793aa9df0f24bc8b905a6
exoad <jackmeng0814@gmail.com>
2024-03-24 13:09:44 -0400

    `add` fix theme chooser layout

--- lib/parts/views/theme_chooser.dart
+++ lib/parts/views/theme_chooser.dart
@@ -1,164 +1,179 @@
 import 'package:flutter/material.dart';
+import 'package:scouting_app_2024/extern/color.dart';
 import 'package:scouting_app_2024/parts/theme.dart';
 import 'package:scouting_app_2024/parts/theme_classifier.dart';
 import 'package:scouting_app_2024/user/user_telemetry.dart';
 import 'package:theme_provider/theme_provider.dart';
 
-class ThemeChooserView extends StatelessWidget {
-  final List<AppTheme> appThemes;
+class _ThemeSelectorTextFrame extends StatelessWidget {
+  final String properName;
+  final Color biContrast;
+  final String author;
+  final bool isDark;
+  final bool isChecked;
+  final IconData icon;
+  final Color selectedColor;
+  final List<Color> scheme;
 
-  const ThemeChooserView({
-    super.key,
-  required this.appThemes});
+  const _ThemeSelectorTextFrame(
+      {required this.properName,
+      required this.biContrast,
+      required this.icon,
+      required this.isChecked,
+      required this.scheme,
+      required this.selectedColor,
+      required this.isDark,
+      required this.author})
+      : assert(scheme.length >
+            0); // AND NOW YOU DONT WANT ME TO USE isNotEmpty and isEmpty ?? FUCK YOU DART!!!!
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Stack(children: <Widget>[
+      if (isChecked)
+        const Align(
+            alignment: Alignment.topRight,
+            child: Icon(Icons.check_rounded)),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Icon(icon, color: isChecked ? selectedColor : biContrast, size: 24),
+          Text.rich(
+            TextSpan(children: <InlineSpan>[
+              TextSpan(
+                  text: " $properName",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  )),
+              if (author !=
+                  "Builtin") // this is so fucking stupid bc its so hardcoded, but its also so necessary yet so unnecessary
+                TextSpan(
+                    text: "\n$author",
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 12,
+                        fontStyle: FontStyle.italic))
+            ]),
+            style: TextStyle(
+                color: isChecked ? selectedColor : biContrast),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 14),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                for (Color color in scheme)
+                  Container(
+                      margin: const EdgeInsets.only(
+                          bottom: 6, left: 4, right: 4),
+                      width: 20,
+                      height: 20,
+                      decoration: BoxDecoration(
+                          color: color,
+                          border:
+                              Border.all(color: biContrast, width: 1),
+                          borderRadius: BorderRadius.circular(4),
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                                spreadRadius: 2,
+                                color: color,
+                                offset: const Offset(0, 1.4),
+                                blurRadius: 8),
+                          ]))
+              ])
+        ],
+      ),
+    ]);
+  }
+}
+
+class ThemeChooserView extends StatelessWidget {
+  final List<AppTheme> appThemes;
+
+  const ThemeChooserView({super.key, required this.appThemes});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> themeWidgets = <Widget>[];
+    for (AppTheme e in appThemes) {
+      bool checked = e.id != ThemeClassifier.of(context).id;
+      _ThemeSelectorTextFrame t = _ThemeSelectorTextFrame(
+          icon: AvailableTheme.of(e.id).icon ?? Icons.palette_rounded,
+          isDark: e.data.brightness == Brightness.dark,
+          isChecked: !checked,
+          scheme: <Color>[
+            e.data.colorScheme.primary,
+            e.data.colorScheme.secondary,
+            e.data.colorScheme.tertiary
+          ],
+          selectedColor: e.data.colorScheme.background.invert(),
+          properName: AvailableTheme.of(e.id).properName,
+          biContrast: e.data.primaryColor.biContrastingColor(),
+          author: AvailableTheme.of(e.id).author);
+      Widget x = checked
+          ? ElevatedButton(
+              style: ButtonStyle(
+                  visualDensity: VisualDensity.comfortable,
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      e.data.primaryColor),
+                  shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8))),
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                      const EdgeInsets.all(14))),
+              onPressed: () {
+                ThemeProvider.controllerOf(context).setTheme(e.id);
+                UserTelemetry().currentModel.selectedTheme =
+                    ThemeClassifier.of(context).id;
+                UserTelemetry().save();
+              },
+              child: t)
+          : OutlinedButton(
+              style: ButtonStyle(
+                  visualDensity: VisualDensity.comfortable,
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                      e.data.iconTheme.color ?? Colors.black),
+                  shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8))),
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(const EdgeInsets.all(14))),
+              onPressed: () {
+                ThemeProvider.controllerOf(context).setTheme(e.id);
+                UserTelemetry().currentModel.selectedTheme =
+                    ThemeClassifier.of(context).id;
+                UserTelemetry().save();
+              },
+              child: t);
+
+      themeWidgets.add(x);
+    }
+    return ScrollConfiguration(
+      behavior: ScrollConfiguration.of(context).copyWith(
+          scrollbars: UserTelemetry().currentModel.showScrollbar),
+      child: Scaffold(
         appBar: AppBar(
-            actions: <Widget>[
-              TextButton.icon(
-                  onPressed: () {
-                    Navigator.of(context)
-                        .pop(); // i debugged this shit a fuck ton, it kept closing the scaffold. reaosn? it was using the original build(BuildContext) which was already gone lmao, fuck they should have nested shadowing rules
-                    UserTelemetry().currentModel.selectedTheme =
-                        ThemeClassifier.of(context).id;
-                    UserTelemetry().save();
-                  },
-                  icon: const Icon(Icons.check_rounded),
-                  label: const Text("Set",
-                      style: TextStyle(fontWeight: FontWeight.bold)))
-            ],
             centerTitle: true,
-            title: const Row(children: <Widget>[
-              Icon(Icons.palette_rounded),
-              SizedBox(width: 10),
-              Text("Theme Library")
+            title: Row(children: <Widget>[
+              const Icon(Icons.palette_rounded),
+              const SizedBox(width: 10),
+              Text("Theme Library (${appThemes.length})")
             ])),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Text.rich(
-                  TextSpan(children: <InlineSpan>[
-                    TextSpan(
-                        text: AvailableTheme.of(
-                                ThemeClassifier.of(context).id)
-                            .properName,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold)),
-                    TextSpan(
-                        text:
-                            ", a Material ${ThemeProvider.themeOf(context).data.useMaterial3 ? "3" : "2"} theme")
-                  ]),
-                  style: const TextStyle(fontSize: 18)),
-              const SizedBox(height: 10),
-              GridView.count(
-                crossAxisCount: appThemes.length,
-                children: <Widget>[
-                  for (AppTheme e in appThemes)
-                    e.id != ThemeClassifier.of(context).id
-                        ? ElevatedButton.icon(
-                            style: ButtonStyle(
-                                visualDensity:
-                                    VisualDensity.comfortable,
-                                backgroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                        e.data.primaryColor),
-                                iconColor:
-                                    MaterialStateProperty.all<Color>(
-                                        e.data.iconTheme.color ??
-                                            Colors.black),
-                                foregroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                        e.data.iconTheme.color ??
-                                            Colors.black),
-                                iconSize:
-                                    MaterialStateProperty.all<double>(30),
-                                shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
-                                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(const EdgeInsets.all(14))),
-                            onPressed: () {
-                              ThemeProvider.controllerOf(context)
-                                  .setTheme(e.id);
-                              UserTelemetry()
-                                      .currentModel
-                                      .selectedTheme =
-                                  ThemeClassifier.of(context).id;
-                              UserTelemetry().save();
-                            },
-                            icon: Column(
-                              children: <Widget>[
-                                Icon(AvailableTheme.of(
-                                  e.id,
-                                ).icon),
-                                const SizedBox(height: 6),
-                                if (e.data.brightness ==
-                                    Brightness.dark)
-                                  const Icon(Icons.nightlight_round,
-                                      size: 14)
-                                else
-                                  const Icon(Icons.wb_sunny_rounded,
-                                      size: 14)
-                              ],
-                            ),
-                            label: Text.rich(TextSpan(children: <InlineSpan>[
-                              TextSpan(
-                                  text: AvailableTheme.of(e.id)
-                                      .properName,
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18)),
-                              const TextSpan(text: "\nBy "),
-                              TextSpan(
-                                  text:
-                                      AvailableTheme.of(e.id).author,
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.w400,
-                                      fontSize: 12,
-                                      fontStyle: FontStyle.italic))
-                            ])))
-                        : OutlinedButton.icon(
-                            style: ButtonStyle(visualDensity: VisualDensity.comfortable, iconColor: MaterialStateProperty.all<Color>(e.data.iconTheme.color ?? Colors.black), foregroundColor: MaterialStateProperty.all<Color>(e.data.iconTheme.color ?? Colors.black), iconSize: MaterialStateProperty.all<double>(30), shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))), padding: MaterialStateProperty.all<EdgeInsetsGeometry>(const EdgeInsets.all(14))),
-                            onPressed: () {
-                              ThemeProvider.controllerOf(context)
-                                  .setTheme(e.id);
-                              UserTelemetry()
-                                      .currentModel
-                                      .selectedTheme =
-                                  ThemeClassifier.of(context).id;
-                              UserTelemetry().save();
-                            },
-                            icon: Column(
-                              children: <Widget>[
-                                Icon(AvailableTheme.of(
-                                  e.id,
-                                ).icon),
-                                const SizedBox(height: 6),
-                                if (e.data.brightness ==
-                                    Brightness.dark)
-                                  const Icon(Icons.nightlight_round,
-                                      size: 14)
-                                else
-                                  const Icon(Icons.wb_sunny_rounded,
-                                      size: 14)
-                              ],
-                            ),
-                            label: Text.rich(TextSpan(children: <InlineSpan>[
-                              TextSpan(
-                                  text: AvailableTheme.of(e.id)
-                                      .properName,
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18)),
-                              const TextSpan(text: "\nBy "),
-                              TextSpan(
-                                  text:
-                                      AvailableTheme.of(e.id).author,
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.w400,
-                                      fontSize: 12,
-                                      fontStyle: FontStyle.italic))
-                            ])))
-                ],
-              ),
-            ]));
+        body: Padding(
+          padding: const EdgeInsets.only(
+              left: 8, right: 8, top: 12),
+          child: GridView.count(
+            physics: const BouncingScrollPhysics(
+                parent: AlwaysScrollableScrollPhysics()),
+            crossAxisCount: 2,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            children: themeWidgets,
+          ),
+        ),
+      ),
+    );
   }
 }
